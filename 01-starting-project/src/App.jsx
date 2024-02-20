e07dd33023566b51747a5f01f6cbbd3b40fe5173
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-20 23:47:12 +0000

    selected styling on tabContent

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,30 +1,36 @@
-import { useState } from "react";
-import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { EXAMPLES } from "./data.js";
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
 
-  // let tabContent = <p>Please select a topic</p>;
-  // if (selectedTopic){
-  //   tabContent=
-  // }
-  // function handleSelect(selectedButton) {
-  //   // selectedButton => 'components','jsx','props,'state'
-  //   setSelectedTopic(selectedButton);
-  //   console.log(tabContent);
-  //   let tabContent=  <div id="tab-content">
-  //   <h3>{EXAMPLES[selectedTopic].title}</h3>
-  //   <p>{EXAMPLES[selectedTopic].description}</p>
-  //   <pre>
-  //     <code>{EXAMPLES[selectedTopic].code}</code>
-  //   </pre>
-  // </div>
-  // }
-  
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
+
+  console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -42,38 +48,35 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <section>
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onSelect={() => handleSelect('components')}
+            >
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onSelect={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onSelect={() => handleSelect('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onSelect={() => handleSelect('state')}
+            >
+              State
+            </TabButton>
           </menu>
-          {!selectedTopic ? <p>Please select a topic</p> : null}
-          {selectedTopic ? (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          ) : null}
-          {/* {!selectedTopic && <p>Please select a topic</p>}
-          { selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )} */}
-
-
+          {tabContent}
         </section>
       </main>
     </div>
