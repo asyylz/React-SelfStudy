83d9c27053669ae950d512b43cecb655e3e88d7a
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-20 05:54:13 +0000

    Folder structure

--- 01-starting-project/src/components/Header/Header.jsx
+++ 01-starting-project/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import reactImg from "../assets/react-core-concepts.png";
+import reactImg from "../../assets/react-core-concepts.png";
+import './Header.css'
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 function getRandom(max) {
   return Math.floor(Math.random() * Math.floor(max));
