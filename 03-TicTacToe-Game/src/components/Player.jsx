f9bd0de161581942204c4d0ddd739fcb073d88e8
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-21 23:32:26 +0000

    Player component edit name functionality

--- 03-TicTacToe-Game/src/components/Player.jsx
+++ 03-TicTacToe-Game/src/components/Player.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+  let playerName = <span className="player-name">{name}</span>;
 
-export default function Player({name,symbol}) {
+  if (isEditing) {
+    playerName = <input type="text" required/>;
+  }
   return (
     <li>
-    <span className="player">
-      <span className="player-name">{name}</span>
-      <span className="player-symbol">{symbol}</span>
-    </span>
-    <button>Edit</button>
-  </li>
-  )
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
 }
-
