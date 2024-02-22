0469e2a67495075de7f908277cc16e4b4ba3c46c
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-22 22:45:11 +0000

    updating gameboard state

--- 03-TicTacToe-Game/src/components/GameBoard.jsx
+++ 03-TicTacToe-Game/src/components/GameBoard.jsx
@@ -1,23 +1,39 @@
+import { useState } from "react";
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
-  [null, null, null]
+  [null, null, null],
 ];
 
 export default function GameBoard() {
-  return(
-  <ol id="game-board">
-    {initialGameBoard.map((row, rowIndex) => (
-      <li key={rowIndex}>
-        <ol>
-          {row.map((playerSymbol, colIndex) => (
-            <li key={colIndex}>
-              <button>{playerSymbol}</button>
-            </li>
-          ))}
-        </ol>
-      </li>
-    ))}
-  </ol>
-  )
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      console.log([...prevGameBoard]);
+      const updatedBoard = [
+        ...prevGameBoard
+      ];
+      console.log(updatedBoard)
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
