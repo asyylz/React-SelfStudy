7998eccc7fc7e9f376f4856f550951fc05b08b5b
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-23 07:44:37 +0000

    state  usage reduced

--- 03-TicTacToe-Game/src/components/GameBoard.jsx
+++ 03-TicTacToe-Game/src/components/GameBoard.jsx
@@ -13,19 +13,6 @@ export default function GameBoard({ onSelectSquare, turns }) {
     gameBoard[row][col] = player;
 
   }
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   function handleSelectSquare(rowIndex, colIndex) {
-  //     setGameBoard((prevGameBoard) => {
-  //       const updatedBoard = [
-  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //       ];
-  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedBoard;
-  //     });
-  //     onSelectSquare();
-  //   }
-  // So we're no longer going to manage our game state in this component. Instead, we're going to lift it up.
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
