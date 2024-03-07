f213137a8d8b785c0dde7390648f6be88cc0e21e
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-07 11:51:32 +0000

    hospital appointment starter files

--- 05-ToDo-App-RGB/src/main.jsx
+++ 05-ToDo-App-RGB/src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
-import "./App.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.querySelector(".container")).render(
   <React.StrictMode>
