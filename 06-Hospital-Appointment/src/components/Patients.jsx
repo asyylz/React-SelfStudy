daea4d36adb2cc906bf863461a58eddb077d18ce
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-08 11:52:06 +0000

    new project structure

--- 06-Hospital-Appointment/src/components/Patients.jsx
+++ 06-Hospital-Appointment/src/components/Patients.jsx
@@ -1,5 +1,3 @@
-import "/src/styles/patient-doctor-page.css";
-import Patient from "./Patient";
 import { useState } from "react";
 import Data from "../helper/Data";
 export default function Patients({
@@ -87,83 +85,5 @@ export default function Patients({
     }
   }
 
-  return 
-    // <div className="container">
-    //   <div className="row">
-    //     <div className="col-md-3">
-    //       <div className="border-bottom p-4">
-    //         <div className="osahan-user text-center">
-    //           <div className="osahan-user-media p-3">
-    //             <h2>{selectedDoctor.expertiseField}</h2>
-    //             <img
-    //               className="mb-3 rounded-pill shadow-sm mt-1"
-    //               src={selectedDoctor.img}
-    //               alt={selectedDoctor.doctorName}
-    //             />
-    //             <div className="osahan-user-media-body">
-    //               <h6 className="mb-2">{selectedDoctor.doctorName}</h6>
-    //               <p className="mb-1">{selectedDoctor.telNumber}</p>
-    //               <p>{selectedDoctor.email}</p>
-    //               <p className="mb-0 text-black font-weight-bold">
-    //                 <button className="button-33" role="button">
-    //                   Contact
-    //                 </button>
-    //               </p>
-    //               <div className="book-appointment">
-    //                 <h4 className="mt-3">Book Appointment</h4>
-    //                 <label htmlFor="appointment-input">Name:</label>
-    //                 <input
-    //                   id="appointment-input"
-    //                   type="text"
-    //                   onChange={(e) => setInput(e.target.value)}
-    //                 />
-    //                 <label htmlFor="appointment-date">Date:</label>
-    //                 <input
-    //                   id="appointment-date"
-    //                   type="date"
-    //                   onChange={(e) => setDate(e.target.value)}
-    //                 />
-    //                 <label htmlFor="appointment-message">Your concerns:</label>
-    //                 <textarea
-    //                   htmlFor="appointment-message"
-    //                   className="p-1"
-    //                   name=""
-    //                   id=""
-    //                   cols="30"
-    //                   rows="10"
-    //                   value={textarea} // Binding the value to the state variable textarea
-    //                   onChange={(e) => setTextarea(e.target.value)}
-    //                 ></textarea>
-    //                 <button
-    //                   className="button-33 m-3"
-    //                   role="button"
-    //                   onClick={handleNewAppointment}
-    //                 >
-    //                   Book
-    //                 </button>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div id="patient-container" className="col-md-9 mt-4">
-    //       <div className="osahan-account-page-right shadow-sm bg-white p-4 h-100">
-    //         <div className="tab-content" id="myTabContent">
-    //           <div
-    //             className="tab-pane fade active show"
-    //             id="offers"
-    //             role="tabpanel"
-    //             aria-labelledby="offers-tab"
-    //           >
-    //             <div className="row mb-4 pb-2">
-    //               <Patient patientList={patientList} />
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-  
+  return;
 }
