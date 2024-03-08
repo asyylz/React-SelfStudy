62c69d3c9f7c4ab47c745202f6c6606d6b9781a2
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-08 12:03:44 +0000

    update

--- 06-HospitalAppointment/src/App.jsx
+++ 06-HospitalAppointment/src/App.jsx
@@ -2,13 +2,51 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <div class="container">
+      <div class="sidebar">
+        <ul>
+          <li>
+            <a href="#">
+              <i class="fas fa-clinic-medical"></i>
+              <div class="title">Brand Name</div>
+            </a>
+            <a href="#">
+              <i class="fas fa-th-large"></i>
+              <div class="title">Dashboard</div>
+            </a>
+            <a href="#">
+              <i class="fas fa-stethoscope"></i>
+              <div class="title">Appointments</div>
+            </a>
+            <a href="#">
+              <i class="fas fa-user-md"></i>
+              <div class="title">Doctors</div>
+            </a>
+            <a href="#">
+              <i class="fas fa-puzzle-piece"></i>
+              <div class="title">Departments</div>
+            </a>
+            <a href="#">
+              <i class="fas fa-hand-holding-usd"></i>
+              <div class="title">Payments</div>
+            </a>
+            <a href="#">
+              <i class="fas fa-cog"></i>
+              <div class="title">Settings</div>
+            </a>
+            <a href="#">
+              <i class="fas fa-question"></i>
+              <div class="title">Help</div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
+   
+  
 }
 
 export default App
