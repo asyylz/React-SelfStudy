f3d081841e8a682fd7e7e42d183b73ac178fca2b
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-15 13:55:55 +0000

    Shopping-Cart  starter files

---
+++ 08-Shopping-Cart/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Shopping
+      </a>
+      <ul>
+        <li>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
