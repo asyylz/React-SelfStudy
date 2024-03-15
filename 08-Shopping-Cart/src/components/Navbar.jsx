55d1555f945ac6ca5b0bad1624cf03c3c7ffea08
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-15 14:09:11 +0000

    navbar

--- 08-Shopping-Cart/src/components/Navbar.jsx
+++ 08-Shopping-Cart/src/components/Navbar.jsx
@@ -2,13 +2,20 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <a href="/" className="site-title">
-        Shopping
+        Shopping Cart
       </a>
       <ul>
+        <li className="active">
+          <a href="/home">Home</a>
+        </li>
         <li>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
+          <a href="/newProduct">New Product</a>
+        </li>
+        <li>
+          <a href="/productList">Product List</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
         </li>
       </ul>
     </nav>
