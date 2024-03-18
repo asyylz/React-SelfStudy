0105dfddf515c2e9de4c63e46a21bc4169043bda
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-18 11:31:40 +0000

    TimerChallenge component

--- 09-/src/components/TimerChallenge.jsx
+++ 09-/src/components/TimerChallenge.jsx
@@ -1,8 +1,22 @@
-export default function TimerChallenge({ title,targetTime }) {
+import { useState, useRef } from "react";
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(null);
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
-      <p className="challenge-time"></p>
+      {timerExpired && <p>You lost</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="">Time is running...</p>
     </section>
   );
 }
