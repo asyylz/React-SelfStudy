5532a71e1972de8fb30b59f9a7f79da80f6766ca
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-18 13:16:11 +0000

    modal window

--- 09-/src/components/TimerChallenge.jsx
+++ 09-/src/components/TimerChallenge.jsx
@@ -6,10 +6,12 @@ export default function TimerChallenge({ title, targetTime }) {
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
   const timer = useRef();
+  const dialog = useRef();
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
     setTimerStarted(true);
     console.log(timer);
@@ -20,10 +22,9 @@ export default function TimerChallenge({ title, targetTime }) {
   }
   return (
     <>
-      <ResultModal />
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You lost</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
