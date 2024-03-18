67e105ee2fcaec5e43225a0a6de96811e5aa6cca
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-18 11:25:42 +0000

    TimerChallenge component

---
+++ 09-/src/components/TimerChallenge.jsx
@@ -0,0 +1,8 @@
+export default function TimerChallenge({ title,targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time"></p>
+    </section>
+  );
+}
