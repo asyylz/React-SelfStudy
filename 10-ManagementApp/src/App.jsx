10f3bb08c2be485ceee9648257ccf79cb5dd4939
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-19 23:05:11 +0000

    managing tasks

--- 10-ManagementApp/src/App.jsx
+++ 10-ManagementApp/src/App.jsx
@@ -8,7 +8,23 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined, // We are doing nothing
     projects: [],
+    tasks: [],
   });
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+  function handleDeleteTask() {}
 
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
@@ -65,7 +81,13 @@ function App() {
   );
 
   let content = (
-    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
   );
 
   if (projectsState.selectedProjectId === null) {
--- 10-ManagementApp/src/components/NewTask.jsx
+++ 10-ManagementApp/src/components/NewTask.jsx
@@ -1,8 +1,27 @@
-export default function NewTask() {
+import { useState } from "react";
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState(second);
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+  function handleClick() {
+    onAdd(enteredTask);
+    setEnteredTask("");
+  }
   return (
     <div className=" flex items-center gap-4">
-      <input type="text" className="w-4 px-2 py-1 rounded-sm bg-stone-200" />
-      <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+      <input
+        onChange={handleChange}
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        value={enteredTask}
+      />
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
     </div>
   );
 }
