359286aff4d424c375cfaa16dcdbeee71bf645b9
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-19 14:15:24 +0000

    button component

--- 10-ManagementApp/src/App.jsx
+++ 10-ManagementApp/src/App.jsx
@@ -1,10 +1,16 @@
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
   return (
     <main className="h-screen my-8 flex gap-8 ">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
