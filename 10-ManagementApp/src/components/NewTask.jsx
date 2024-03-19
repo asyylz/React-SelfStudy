c8f94534e21364e28868da9429ea0fc6c9eed576
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-19 23:15:57 +0000

    managing tasks

--- 10-ManagementApp/src/components/NewTask.jsx
+++ 10-ManagementApp/src/components/NewTask.jsx
@@ -1,30 +1,31 @@
-import NewTask from './NewTask.jsx';
+import { useState } from "react";
 
-export default function Tasks({ tasks, onAdd, onDelete }) {
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    onAdd(enteredTask);
+    setEnteredTask("");
+  }
+
   return (
-    <section>
-      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-      <NewTask onAdd={onAdd} />
-      {tasks.length === 0 && (
-        <p className="text-stone-800 my-4">
-          This project does not have any tasks yet.
-        </p>
-      )}
-      {tasks.length > 0 && (
-        <ul className="p-4 mt-8 rounded-md bg-stone-100">
-          {tasks.map((task) => (
-            <li key={task.id} className="flex justify-between my-4">
-              <span>{task.text}</span>
-              <button
-                className="text-stone-700 hover:text-red-500"
-                onClick={() => onDelete(task.id)}
-              >
-                Clear
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </section>
+    <div className="flex items-center gap-4">
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
+    </div>
   );
 }
