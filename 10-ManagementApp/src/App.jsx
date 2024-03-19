41bc12b769bfc7b211def3b419bcd30e46f19504
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-19 14:35:29 +0000

    App component

--- 10-ManagementApp/src/App.jsx
+++ 10-ManagementApp/src/App.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
-import ProjectsSidebar from "./components/ProjectsSidebar";
-import { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+
 function App() {
   const [projectsState, setProjectsState] = useState({
-    selectedProject: undefined,
+    selectedProjectId: undefined, // We are doing nothing
     projects: [],
   });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null, // We are adding a project
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
-
 export default App;
