2086b93e52dcfc1bc8ed1a85384a3b338546aa9f
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-19 22:27:58 +0000

    Handling Project Deletion

--- 10-ManagementApp/src/App.jsx
+++ 10-ManagementApp/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     selectedProjectId: undefined, // We are doing nothing
     projects: [],
   });
+
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
       return {
@@ -47,12 +48,25 @@ function App() {
       };
     });
   }
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
 
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
