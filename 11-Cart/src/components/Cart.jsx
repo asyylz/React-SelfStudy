a8656ee98fd7393d91c4aec5851c2efb57936546
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-20 09:58:30 +0000

    context api

--- 11-Cart/src/components/Cart.jsx
+++ 11-Cart/src/components/Cart.jsx
@@ -1,9 +1,9 @@
 import { CartContext } from "../store/shopping-cart-context";
 import { useContext } from "react";
 CartContext;
-export default function Cart({ onUpdateItemQuantity }) {
+export default function Cart() {
   //const cartCtx = useContext(CartContext);
-  const { items } = useContext(CartContext); // destructed cartCtx
+  const { items, updateItemQuantity } = useContext(CartContext); // destructed cartCtx
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -25,11 +25,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
