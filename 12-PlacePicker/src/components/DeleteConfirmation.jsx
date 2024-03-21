834f6b2ec4471153d5713b8aabcafb46e4e588f3
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-21 16:46:48 +0000

    cleanup funciton in useEffect

--- 12-PlacePicker/src/components/DeleteConfirmation.jsx
+++ 12-PlacePicker/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log('timerset')
+    const timer = setTimeout(function () {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+      console.log('clean timer')
+    };
+  }, [onConfirm ]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
