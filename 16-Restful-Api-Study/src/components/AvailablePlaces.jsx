87b401e123773423fe43671482134ce38b5a5b9a
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-01 22:35:35 +0100

    navigator

--- 16-Restful-Api-Study/src/components/AvailablePlaces.jsx
+++ 16-Restful-Api-Study/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -17,14 +18,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
           const error = new Error("Failed to fetch places");
           throw error;
         }
-        setAvailablePlaces(resData.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || " Could not fetch places, please try again later.",
         });
+        setIsFetching(false);
       }
-      setIsFetching(false);
+      //setIsFetching(false); // too early  here because navigator does not take await
     }
     fetchPlace();
   }, []);
