58102ec9086154aa1173a1f2ff55987c9507ec27
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-01 22:29:09 +0100

    error message

--- 16-Restful-Api-Study/src/components/AvailablePlaces.jsx
+++ 16-Restful-Api-Study/src/components/AvailablePlaces.jsx
@@ -1,20 +1,37 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlace() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+        if (!response.ok) {
+          const error = new Error("Failed to fetch places");
+          throw error;
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || " Could not fetch places, please try again later.",
+        });
+      }
       setIsFetching(false);
     }
     fetchPlace();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred !" message={error.message} />;
+  }
 
   return (
     <Places
