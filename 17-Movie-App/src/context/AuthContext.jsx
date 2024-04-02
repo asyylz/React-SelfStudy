fbae94997a161e307cf6843cfebe4713f4639718
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-02 09:43:56 +0100

    auth context

---
+++ 17-Movie-App/src/context/AuthContext.jsx
@@ -0,0 +1,21 @@
+import { Children, createContext } from "react";
+
+// create context
+const AuthContext = createContext();
+
+// context provider
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState("");
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+//consumer with custom hook
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+export default AuthContextProvider;
