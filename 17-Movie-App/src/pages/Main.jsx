e0bdab64d79dc7024b8277ea456f08dbee890143
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-03 11:08:18 +0100

    movie card

--- 17-Movie-App/src/pages/Main.jsx
+++ 17-Movie-App/src/pages/Main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 const Main = () => {
-  const { movies } = useMovieContext();
+  const { movies, loading } = useMovieContext();
   console.log(movies);
-  return <div>Main</div>;
+  return (
+    <div className="flex justify-center flex-wrap">
+      {loading ? (
+        <h2 className=" text-2xl text-red-700 dark:text-white">Loading...</h2>
+      ) : (
+        movies.map((movie) => <MovieCard />)
+      )}
+    </div>
+  );
 };
-
 export default Main;
