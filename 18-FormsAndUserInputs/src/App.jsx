2793bd0cb5cb3c20be3378a4c1ec39b8556173d6
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-03 15:55:54 +0100

    outsourced input component

--- 18-FormsAndUserInputs/src/App.jsx
+++ 18-FormsAndUserInputs/src/App.jsx
@@ -1,15 +1,16 @@
 import Header from "./components/Header.jsx";
 import Signup from "./components/SignUp.jsx";
 import RefBaseLogin from "./components/RefBaseLogin.jsx";
-import StateLogin from "./components/StateLogin.jsx";// switch between the login components login component
+import StateLogin from "./components/StateLogin.jsx"; // switch between the login components login component
 
 function App() {
   return (
     <>
       <Header />
       <main>
-       {/* <RefBaseLogin /> */}
-       <Signup />   
+        {/* <RefBaseLogin /> */}
+        {/* <Signup />    */}
+        <StateLogin />
       </main>
     </>
   );
