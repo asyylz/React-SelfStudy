6235e30a21f28aa8dfc21eee22c8ff41d00e0642
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-03 11:22:12 +0100

    movie details

--- 17-Movie-App/src/components/MovieCard.jsx
+++ 17-Movie-App/src/components/MovieCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const defaultImage =
-  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"; //* resim yoksa default resmi göster diyecez
+  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"; //*  default image
 
 const MovieCard = ({ id, poster_path, overview, vote_average, title }) => {
   const { currentUser } = useAuthContext();
+  const navigate = useNavigate();
   const tagColor =
     vote_average >= 8 ? "green" : vote_average >= 6 ? "orange" : "red";
 
   return (
-    <div className="movie" id="container">
+    <div
+      className="movie"
+      id="container"
+      onClick={() => navigate("/detailes/" + id)}
+    >
       <img
         loading="lazy"
         src={
