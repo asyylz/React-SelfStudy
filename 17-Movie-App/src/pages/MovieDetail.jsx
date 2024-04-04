f01d8b10ad6822df9352b3bd94e56b8cef920db0
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-04 09:53:16 +0100

    movie details

--- 17-Movie-App/src/pages/MovieDetail.jsx
+++ 17-Movie-App/src/pages/MovieDetail.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+
 const MovieDetail = () => {
-  const {id} = useParams
+  const { id } = useParams;
+  const [movieDetail, setMovieDetail] = useState({});
+  const [videoKey, setVideoKey] = useState("");
+
+  const movieDetailUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${
+    import.meta.env.VITE_APP_TMDB_KEY
+  }`;
+  const movieVideo = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${
+    import.meta.env.VITE_APP_TMDB_KEY
+  }`;
+
   return <div>MovieDetail</div>;
 };
 
