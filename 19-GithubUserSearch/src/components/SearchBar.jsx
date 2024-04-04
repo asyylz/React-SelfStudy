801d8d3530c0f1042128e07cdbb0c8ffce29e064
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-04 13:48:36 +0100

    update

--- 19-GithubUserSearch/src/components/SearchBar.jsx
+++ 19-GithubUserSearch/src/components/SearchBar.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react/cjs/react.production.min";
+import { useState } from "react";
 import { isValid } from "./validate";
 export default function SearchBar() {
   const [enteredSearch, setEnteredSearch] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
-    
+
     if (isValid(enteredSearch)) {
       return;
     }
@@ -13,12 +13,20 @@ export default function SearchBar() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={enteredSearch}
-        onChange={(event) => setEnteredSearch(event.target.value)}
-      />
-      <button type="button">Search</button>
+      <div className="control no-margin">
+        <label htmlFor="usersearch">
+          Enter you user name you would like to search
+        </label>
+        <input
+          id="usersearch"
+          type="text"
+          value={enteredSearch}
+          onChange={(event) => setEnteredSearch(event.target.value)}
+        />
+      </div>
+      <p className="form-actions">
+        <button>Search</button>
+      </p>
     </form>
   );
 }
