f7d2b029510d3fa6db5ed53a031f1775fc6e477f
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-21 22:46:24 +0100

    Replace Redux with Context API: removed Redux imports and implementations, added Context API imports and implementations, and updated components to use Context API instead of Redux.

--- 20-ReplaceRedux/src/containers/Products.js
+++ 20-ReplaceRedux/src/containers/Products.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
 
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
-
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+import { ProductsContext } from '../context/products-context';
+const Products = (props) => {
+  const productList = useContext(ProductsContext).products;
   return (
     <ul className="products-list">
-      {productList.map(prod => (
+      {productList.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
